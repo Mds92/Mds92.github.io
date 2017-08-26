d68c0cb2f237c2d32107edda00654d8f6dff0547
Mohammad Dayyan <mds_soft@yahoo.com>
2017-08-26 23:01:50 +0430

    1396-06-04

---
+++ MdsDateTimePickerSample/src/app/app.module.ts
@@ -0,0 +1,17 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { MdsDatetimePickerModule } from 'mds.angular.datetimepicker';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule, MdsDatetimePickerModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
